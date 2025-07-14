4578fb9513cc353e3d68f698d9265a3cd5f48ed6
duressa@2022 <duressashukuri2022@gmail.com>
2025-07-14 16:33:28 +0300

    dashboard,settings,and teams pages are created

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-=======
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
->>>>>>> 539ff0ae11b68bc35483d4dd702c36cc5df696aa
 }
