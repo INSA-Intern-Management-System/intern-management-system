1dc7df27d7386d35b185fee9845388743a1cfd2e
duressa@2022 <duressashukuri2022@gmail.com>
2025-07-23 16:24:21 +0300

    service:message-service fixed

--- Backend/message-service/src/main/java/com/example/message_service/repository/UserJpaRepos.java
+++ Backend/message-service/src/main/java/com/example/message_service/repository/UserJpaRepos.java
@@ -1,5 +1,10 @@
 package com.example.message_service.repository;
 
-public interface UserJpaRepos {
+import com.example.message_service.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+public interface UserJpaRepos extends JpaRepository<User, Long> {
+    List<User> findAllByIdIn(List<Long> ids);
 }
--- Backend/message-service/src/main/java/com/example/message_service/repository/UserReposInterface.java
+++ Backend/message-service/src/main/java/com/example/message_service/repository/UserReposInterface.java
@@ -1,5 +1,9 @@
 package com.example.message_service.repository;
 
-public interface UserReposInterface {
+import com.example.message_service.model.User;
+import java.util.List;
 
+public interface UserReposInterface {
+    User getUser(Long id);
+    List<User> getUsersByIds(List<Long> ids);
 }
